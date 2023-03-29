d94257676195446549a485fbec75c4f3ddd7a87c
piroyoung <piroyoung@users.noreply.github.com>
2023-03-29 13:01:01 +0900

    add: example

--- example/main.go
+++ example/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"goaoai"
 	"os"
 )
 
 func main() {
-	resourceName := "resourceName"
-	deploymentName := "deploymentName"
-	apiVersion := "apiVersion"
+	resourceName := "example-aoai-02"
+	deploymentName := "gpt-35-turbo-0301"
+	apiVersion := "2023-03-15-preview"
 	accessToken := os.Getenv("AZURE_OPENAI_API_KEY")
 
 	client := goaoai.New(resourceName, deploymentName, apiVersion, accessToken)
 
-	result, _ := client.Completion("Negate the following sentence.The price for bubblegum increased on thursday.\n\n Negated Sentence:", 50)
-	fmt.Println(result)
+	result, _ := client.Completion("I have a dream that one day on", 50)
+	if jsonString, err := json.MarshalIndent(result, "", "\t"); err == nil {
+		fmt.Println(string(jsonString))
+	}
 }
